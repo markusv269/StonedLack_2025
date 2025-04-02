bd0a6dad8db50ac1fb7f5f0c43d86db8ed5357f4
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-04-02 08:52:02 +0000

    new file:   .github/workflows/update_drafts.yaml 	new file:   update_complete_drafts.py

---
+++ update_complete_drafts.py
@@ -0,0 +1,48 @@
+import requests
+import os
+from sleeper_wrapper import League, Drafts
+import json
+from config import REDLEAGUES, DYNLEAGUES
+
+
+league_ids = REDLEAGUES + DYNLEAGUES
+
+drafts_dir = "DATA_PERMANENT/_{}/DRAFT_COMP"
+# matchup_dir = "DATA_PERMANENT/_{}/MATCHUPS/{}"
+picks_dir = "DATA_PERMANENT/_{}/PICKS"
+# roster_dir = "DATA_PERMANENT/_{}/ROSTERS/{}"
+
+# get NFL-State
+state_url = "https://api.sleeper.app/v1/state/nfl"
+state_data = requests.get(state_url).json()
+
+nfl_week = state_data.get("week")
+nfl_season_type = state_data.get("season_type")
+nfl_season = state_data.get("season")
+
+# Verzeichnisse erstellen, falls sie nicht existieren
+# os.makedirs(matchup_dir.format(nfl_season, nfl_week), exist_ok=True)
+# os.makedirs(roster_dir.format(nfl_season, nfl_week), exist_ok=True)
+
+for league_id in league_ids:
+    league = League(league_id)
+    drafts = league.get_all_drafts()
+    
+    
+    if drafts:
+        draft_id = drafts[0]["draft_id"]
+        draft = Drafts(draft_id)
+        draft_data = draft.get_specific_draft()
+        status = draft_data["status"]
+        draft_season = draft_data["season"]
+        if status == "complete" and draft_season == nfl_season:
+            all_picks = draft.get_all_picks()
+            
+            # JSON-Dateien speichern
+            os.makedirs(drafts_dir.format(nfl_season), exist_ok=True)
+            with open(f"{drafts_dir.format(nfl_season)}/{draft_id}.json", "w", encoding="utf-8") as draft_file:
+                json.dump(draft_data, draft_file, indent=4)
+
+            os.makedirs(picks_dir.format(nfl_season), exist_ok=True)
+            with open(f"{picks_dir.format(nfl_season)}/{draft_id}.json", "w", encoding="utf-8") as pick_file:
+                json.dump(all_picks, pick_file, indent=4)
