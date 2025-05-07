e3e2f47b2b12c90f9d5bee4057f53d640ab02881
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-05-07 18:59:15 +0000

    modified:   VIEWS/DYNASTY/DYN_drafts.py 	modified:   assets/styles.css 	modified:   utils.py

--- VIEWS/DYNASTY/DYN_drafts.py
+++ VIEWS/DYNASTY/DYN_drafts.py
@@ -1,3 +1,11 @@
+import streamlit as st
 from config import DYNLEAGUES
-from utils import display_drafts
-display_drafts(DYNLEAGUES)
+from utils import display_draft
+
+st.write("### Draftübersicht")
+st.write('''Die Draftübersicht wurde auf die neue Saison 2025 umgestellt. 
+Nur Ligen, die zur neuen Saison einen Draft eingestellt haben, werden nun angezeigt. 
+Alle Ligen können den Ligenübersichten entnommen werden. 
+''')
+for league_id in DYNLEAGUES:
+    display_draft(league_id)
--- utils.py
+++ utils.py
@@ -18,137 +18,110 @@ def load_json(file_path):
             return json.load(file)
     return None
 
+def metric_box(label, value):
+    st.markdown(
+        f"""
+        <div class="metric-box">
+            <div class="label">{label}</div>  
+            <div class="value">{value}</div>
+        </div>
+        """,
+        unsafe_allow_html=True
+)
+
 @st.cache_data(ttl=900)
-def display_drafts(league_ids):
-    st.write("### Draftübersicht")
-    st.write('''Die Draftübersicht wurde auf die neue Saison 2025 umgestellt. 
-    Nur Ligen, die zur neuen Saison einen Draft eingestellt haben, werden nun angezeigt. 
-    Alle Ligen können den Ligenübersichten entnommen werden. 
-    ''')
-    for league_id in league_ids:
-        league = SleeperLeague(league_id)
-        # league_data = league.get_league()
+def display_draft(league_id):
+    league = SleeperLeague(league_id)
+    league_data = league.get_league_info()
+    st.write(f"#### {league_data['name']}")
+    draft_ids = league.get_draft_ids()
+    for draft_id in draft_ids:
+        draft = SleeperDraft(draft_id)
+        draft_data = draft.get_draft_info()
+        # st.write(draft_data)
+        picks = draft.get_all_picks()
+        
+        draft_order = draft_data.get("draft_order", {})
+        draft_time = draft_data.get("start_time", None)
+        draft_type = draft_data["settings"].get("player_type")
+        draft_mode = draft_data["type"]
 
-        try:
-            league_data = league.get_league_info()
-        except requests.exceptions.HTTPError as e:
-            st.error(f"Fehler beim Abrufen der Liga {league_id}: {e}")
-            continue  # Liga überspringen, falls sie nicht existiert oder ein Fehler auftritt
+        if draft_type == 1:
+            draft_typ = "Rookie Draft"
+        elif draft_type == 2:
+            draft_typ = "Veteran Draft"
+        else:
+            draft_typ = "Draft"
 
-        if not isinstance(league_data, dict):  
-            st.error(f"Liga {league_id} existiert nicht mehr oder ungültige Antwort erhalten.")
-            continue
-        # roster_data = league.get_rosters()
-        st.write(f"#### {league_data['name']}")
-        league_draft_id = league.get_draft_ids()
-        for draft_id in league_draft_id:
-            draft = SleeperDraft(draft_id)
-            draft_data = draft.get_draft_info()
-            
-            if draft_data["season"] == "2025":
-                # Falls der Draft-Status „complete“ ist, aus Datei laden
-                if draft_data["status"] == "complete":
-                    picks = load_json(f"{picks_dir}/{draft_id}.json")
-                else:
-                    picks = draft.get_all_picks()
-                
-                draft_order = draft_data.get("draft_order", {})
-                draft_time = draft_data.get("start_time", None)
-                draft_type = draft_data["settings"].get("player_type")
-                draft_mode = draft_data["type"]
+        if draft_time:
+            draft_time /= 1000  # Millisekunden in Sekunden
+            draft_time_utc = datetime.fromtimestamp(draft_time, tz=timezone.utc)  # UTC-Zeit
+            draft_time_mesz = draft_time_utc.astimezone(ZoneInfo("Europe/Berlin"))  # In MESZ umwandeln
+            draft_time_show = draft_time_mesz.strftime("%d.%m.%Y %H:%M")
+        else:
+            draft_time_show = "TBD"
 
-                if draft_type == 1:
-                    draft_typ = "Rookie Draft"
-                elif draft_type == 2:
-                    draft_typ = "Veteran Draft"
-                else:
-                    draft_typ = "Draft"
+        latest_pick = picks[-1] if picks else None
+        
+        if latest_pick:
+            pick_data = [
+                latest_pick["metadata"].get('first_name', 'Unknown'),
+                latest_pick["metadata"].get('last_name', 'Unknown'),
+                latest_pick["metadata"].get('position', 'Unknown'),
+                latest_pick["metadata"].get('team', 'Unknown'),
+                latest_pick["round"],
+                latest_pick["draft_slot"],
+                latest_pick["picked_by"]
+            ]
+        else:
+            pick_data = None
 
-                if draft_time:
-                    draft_time /= 1000  # Millisekunden in Sekunden
-                    draft_time_utc = datetime.fromtimestamp(draft_time, tz=timezone.utc)  # UTC-Zeit
-                    draft_time_mesz = draft_time_utc.astimezone(ZoneInfo("Europe/Berlin"))  # In MESZ umwandeln
-                    draft_time_show = draft_time_mesz.strftime("%d.%m.%Y %H:%M")
-                else:
-                    draft_time_show = "TBD"
+        st.write(f"**{draft_typ} {draft_data['season']}**")
 
-                latest_pick = picks[-1] if picks else None
-                
-                if latest_pick:
-                    pick_data = [
-                        latest_pick["metadata"].get('first_name', 'Unknown'),
-                        latest_pick["metadata"].get('last_name', 'Unknown'),
-                        latest_pick["metadata"].get('position', 'Unknown'),
-                        latest_pick["metadata"].get('team', 'Unknown'),
-                        latest_pick["round"],
-                        latest_pick["draft_slot"],
-                        latest_pick["picked_by"]
-                    ]
-                else:
-                    pick_data = None
+        col1, col2, col3, col4 = st.columns([2,2,2,3])
+        with col1:
+            metric_box("Start", draft_time_show)
+        with col2:
+            if draft_data["status"] == "complete":
+                st.success("Draft abgeschlossen")
+            elif draft_data["status"] == "pre_draft":   
+                st.error("Draft noch nicht gestartet")
+            elif draft_data["status"] == "drafting":   
+                st.warning("Draft läuft")
+            else:
+                st.warning(draft_data["status"])
+        with col3:
+            metric_box("Draftmodus", draft_mode)   
+        with col4: 
+            metric_box("Draft-URL", f'https://sleeper.com/draft/nfl/{draft_id}')
+    #         with st.expander("Draftdetails anzeigen"):
+    #             col5, col6 = st.columns([1, 4])
+    #             with col5:
+    #                 st.write("Latest Pick")
+    #             with col6:
+    #                 if pick_data:
+    #                     user = User(pick_data[6])
+    #                     user_name = user.get_display_name()
+    #                     st.write(f"**{user_name}:** {pick_data[0]} {pick_data[1]} ({pick_data[2]}, {pick_data[3]}), Pick {pick_data[4]}.{pick_data[5]} ")
+    #                 else:
+    #                     st.write("--")
 
-                st.write(f"**{draft_typ} {draft_data['season']}**")
-
-                col1, col2, col3, col4 = st.columns([2,2,2,3])
-                with col1:
-                    st.metric("Start", draft_time_show)
-                with col2:
-                    if draft_data["status"] == "complete":
-                        st.success("Draft abgeschlossen")
-                    elif draft_data["status"] == "pre_draft":   
-                        st.error("Draft noch nicht gestartet")
-                    elif draft_data["status"] == "drafting":   
-                        st.warning("Draft läuft")
-                    else:
-                        st.warning(draft_data["status"])
-                with col3:
-                    st.metric("Draftmodus", draft_mode)   
-                with col4: 
-                    # st.metric("Draft-URL", f"https://sleeper.com/draft/nfl/{draft_id}")
-                    st.markdown(
-                        f"""
-                        <div class="metric-box">
-                            <div class="label">Draft-URL</div>  
-                            <div class="value">
-                                <a href="https://sleeper.com/draft/nfl/{draft_id}" target="_blank">
-                                    https://sleeper.com/draft/nfl/{draft_id}
-                                </a>
-                            </div>
-                        </div>
-                        """,
-                        unsafe_allow_html=True
-                    )
-                    
-                # st.components.v1.iframe(f"https://sleeper.com/draft/nfl/{draft_id}", width=800, height=600)
-
-                with st.expander("Draftdetails anzeigen"):
-                    col5, col6 = st.columns([1, 4])
-                    with col5:
-                        st.write("Latest Pick")
-                    with col6:
-                        if pick_data:
-                            user = User(pick_data[6])
-                            user_name = user.get_display_name()
-                            st.write(f"**{user_name}:** {pick_data[0]} {pick_data[1]} ({pick_data[2]}, {pick_data[3]}), Pick {pick_data[4]}.{pick_data[5]} ")
-                        else:
-                            st.write("--")
-
-                    # Draftorder in einer Tabelle anzeigen
-                    if draft_order:
-                        draft_list = []
-                        for user_id, draft_pos in draft_order.items():
-                            user = User(user_id)
-                            user_name = user.get_display_name()
-                            draft_list.append({"Draft Position": draft_pos, "Manager": user_name})
-                        col9, col10 = st.columns([1,4])
-                        with col9:
-                            st.write("Draftorder")
-                        with col10:
-                            # Sortieren nach Draft-Position
-                            draft_df = pd.DataFrame(draft_list).sort_values(by="Draft Position")
-                            st.table(draft_df.set_index("Draft Position"))
-                    else:
-                        st.write("No draft order available.")
+    #             # Draftorder in einer Tabelle anzeigen
+    #             if draft_order:
+    #                 draft_list = []
+    #                 for user_id, draft_pos in draft_order.items():
+    #                     user = User(user_id)
+    #                     user_name = user.get_display_name()
+    #                     draft_list.append({"Draft Position": draft_pos, "Manager": user_name})
+    #                 col9, col10 = st.columns([1,4])
+    #                 with col9:
+    #                     st.write("Draftorder")
+    #                 with col10:
+    #                     # Sortieren nach Draft-Position
+    #                     draft_df = pd.DataFrame(draft_list).sort_values(by="Draft Position")
+    #                     st.table(draft_df.set_index("Draft Position"))
+    #             else:
+    #                 st.write("No draft order available.")
 
 BASE_ID = st.secrets["airtable"]["base_id"]
 AIRTABLE_API_KEY = st.secrets["airtable"]["api_key"]
