4d17f0865d9f933b9e754efc0e01490ebd54eec7
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-07-17 08:40:34 +0000

    modified:   SLR2025.py

--- SLR2025.py
+++ SLR2025.py
@@ -110,7 +110,7 @@ with right:
     mitspieler = st.checkbox("Ich möchte mit jemandem zusammenspielen")
 
     with st.form("Anmeldung SLR 2025"):
-        sleeper_name = st.text_input(":moon: Dein Sleeper-Name (Pflichtfeld)", key="sleeper")
+        sleeper_name = st.text_input(":material/moon: Dein Sleeper-Name (Pflichtfeld)", key="sleeper")
         discord_name = st.text_input("Dein Discord-Name (Pflichtfeld)", key="discord")
 
         mitspieler_names = []
