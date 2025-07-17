e029afe19042287281f60b57e74ea618bd9d5070
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-07-17 11:05:37 +0000

    modified:   SLR2025.py

--- SLR2025.py
+++ SLR2025.py
@@ -177,7 +177,7 @@ with right:
         email = ""
         schluessel_input = ""
         if mode == "Anmeldung":
-            email = st.text_input("Deine E-Mail-Adresse für Schlüsselzusendung (Optional)", key="email")
+            email = st.text_input("✉️ Deine E-Mail-Adresse für Schlüsselzusendung (Optional)", key="email")
         else:
             schluessel_input = st.text_input("🔐 Anmeldeschlüssel", help="Nur erforderlich, wenn du deine Anmeldung ändern willst.")
 
