26975fd1d02a694a54b37677db0c06bc97b212b0
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-08-17 20:44:46 +0000

    modified:   VIEWS/REDRAFT/RED_drafts.py

--- VIEWS/REDRAFT/RED_drafts.py
+++ VIEWS/REDRAFT/RED_drafts.py
@@ -1,6 +1,13 @@
 from config import REDLEAGUES_2025
 from utils import display_draft
+from sleeper_wrapper import League
 
 red_leagues = list(reversed(REDLEAGUES_2025.keys()))
-for league_id in red_leagues[:-4]:  # Exclude last 3 leagues
+drafting_leagues = []
+for league_id in red_leagues:  # Last 3 leagues are drafting leagues
+    league = League(league_id)
+    data = league.get_league()
+    if data['status'] != 'pre_draft':
+        drafting_leagues.append(league_id)
+for league_id in drafting_leagues:  # Exclude last 3 leagues
     display_draft(league_id)
