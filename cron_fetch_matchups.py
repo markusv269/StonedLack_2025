598bf487e416659e6719e789c6d8a11cfc0d8e4e
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-12-30 22:35:41 +0100

    Update current week variable to 17

--- cron_fetch_matchups.py
+++ cron_fetch_matchups.py
@@ -28,7 +28,7 @@ league_ids = [l["league_id"] for l in leagues_response.data]
 
 SLEEPER_URL_TEMPLATE = "https://api.sleeper.app/v1/league/{league_id}/matchups/{week}"
 BATCH_SIZE = 500
-current_week = 17
+
 for league_id in league_ids:
     url = SLEEPER_URL_TEMPLATE.format(league_id=league_id, week=current_week)
     resp = requests.get(url)
